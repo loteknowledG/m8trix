210ded57d2116f12727b34fa968fa01004b3fc6f
loteknowledG <loteknowledG@gmail.com>
2021-11-05 01:14:59 -0400

    Updates

--- src/atoms/drawerOpenState.js
+++
@@ -1,8 +0,0 @@
-import { atom } from 'recoil'
-
-export const drawerOpenState = atom({
-  key: 'drawerOpenState', // unique ID (with respect to other atoms/selectors)
-  default: false // default value (aka initial value)
-})
-export default drawerOpenState
-
--- src/atoms/pixState.js
+++
@@ -1,6 +0,0 @@
-import { atom } from 'recoil'
-export const pixState = atom({
-  key: 'pixState',
-  default: []
-})
-export default pixState
